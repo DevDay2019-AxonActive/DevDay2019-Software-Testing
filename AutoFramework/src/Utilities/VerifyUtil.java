797f6473f95073318043add4f1266fb5c593106e
Linh Vuong <linhvuong89@gmail.com>
2019-11-14 18:41:12 +0700

    remove redundant code

--- AutoFramework/src/Utilities/VerifyUtil.java
+++ AutoFramework/src/Utilities/VerifyUtil.java
@@ -8,36 +8,16 @@ import Test.web.AbstractBaseTest;
 public class VerifyUtil extends AbstractBaseTest
 {
 
-    public static boolean isElementDisplayed(WebElement element)
-    {
-        return element.isDisplayed();
-    }
-
-    public static void verifyElementPresent (WebElement element)
+    public static void verifyElementDisplay (WebElement element)
     {
         try
         {
-            Assert.assertTrue(isElementDisplayed(element));
+            Assert.assertTrue(element.isDisplayed());
         }
         catch (AssertionError ae)
         {
             getVerificationErrors().append(ae);
         }
 
-    }
-    public static boolean verifyEqual(Object objectExpected, Object objectDetected)
-    {
-        boolean testResult = false;
-        try
-        {
-            Assert.assertEquals(objectDetected, objectExpected, objectExpected + " not equals to " + objectDetected);
-            testResult = true;
-        }
-        catch (AssertionError e)
-        {
-            getVerificationErrors().append(e);
-        }
-        System.out.println(objectExpected+ " is equal to " + objectDetected);
-        return testResult;
     }
 }
